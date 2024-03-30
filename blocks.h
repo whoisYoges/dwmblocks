7c02820727eb5fd99cbf1cbfc4e1aa2b547dc857
Castor <whoisyoges@castorisdead.xyz>
2024-03-30 23:23:34 +0545

    Configured: brightness, battery, speeker, microphone and datetime modules.

---
+++ blocks.h
@@ -0,0 +1,13 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{" ", "sb-brightness", 60, 5},
+	{"", "sb-battery", 5, 0},
+	{"", "sb-speeker", 5, 6},
+	{"", "sb-microphone", 5, 7},
+	{" ", "date '+%a %Y-%m-%d %H:%M'", 60, 0},
+};
+
+//sets delimiter between status commands. NULL character ('\0') means no delimiter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
