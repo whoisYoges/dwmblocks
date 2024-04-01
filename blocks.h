0fe715112d3f8b0c06a41c633628ee2ba28dafa9
Castor <whoisyoges@castorisdead.xyz>
2024-04-01 12:39:10 +0545

    use brillo as brightness controller, updated update interval for brightness aand timedate

--- blocks.h
+++ blocks.h
@@ -1,11 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{" ", "sb-brightness", 60, 5},
+	{" ", "sb-brightness", 0, 5},
 	{"", "sb-battery", 5, 0},
 	{"", "sb-speeker", 5, 6},
 	{"", "sb-microphone", 5, 7},
-	{" ", "date '+%a %Y-%m-%d %H:%M'", 60, 0},
+	{" ", "date '+%a %Y-%m-%d %H:%M'", 30, 0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
